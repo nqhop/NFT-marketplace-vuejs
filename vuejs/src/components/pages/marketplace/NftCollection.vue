89b3d7b7ff2bda051641e37f2bafe7cd5a99c6d7
Nguyễn Quốc Hợp <hop35889@gmail.com>
2024-01-10 23:32:54 +0700

    nfts

--- vuejs/src/components/pages/marketplace/NftCollection.vue
+++ vuejs/src/components/pages/marketplace/NftCollection.vue
@@ -1,10 +1,17 @@
 <template>
-    Nft Collecitons    
+    <div class="grid wide">
+    <div class="row">
+      <div class="col l-4 m-4 c-12" v-for="index in 12" :key="index">
+        <category-card-vue/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import CategoryCardVue from '@/components/ui/category/CategoryCard.vue';
 export default {
-    
+    components: {CategoryCardVue}
 }
 </script>
 
--- vuejs/src/components/pages/marketplace/NftList.vue
+++ vuejs/src/components/pages/marketplace/NftList.vue
@@ -1,18 +1,35 @@
 <template>
   ndtList
-  <VueAwesomePaginate
-    :total-items="200"
-    :items-per-page="5"
-    :max-pages-shown="5"
-    v-model="currentPage"
-    :on-click="onClickHandler"
-    :show-ending-buttons="true"
-    :show-breakpoint-buttons="false"
-  />
+  <!-- <div class="grid wide">
+    <div class="row">
+      <div class="col l-4 m-4 c-12" v-for="index in 12" :key="index">
+        <the-nft-vue />
+      </div>
+    </div>
+  </div> -->
+
+  <ul>
+    <li v-for="index in 12" :key="index">
+      <the-nft-vue />
+    </li>
+  </ul>
+
+  <div class="paginate">
+    <VueAwesomePaginate
+      :total-items="200"
+      :items-per-page="5"
+      :max-pages-shown="5"
+      v-model="currentPage"
+      :on-click="onClickHandler"
+      :show-ending-buttons="true"
+      :show-breakpoint-buttons="false"
+    />
+  </div>
 </template>
 
 <script>
 import { VueAwesomePaginate } from "vue-awesome-paginate";
+import TheNftVue from "@/components/ui/nft/TheNft.vue";
 
 export default {
   methods: {
@@ -25,9 +42,23 @@ export default {
       currentPage: 1,
     };
   },
-  components: { VueAwesomePaginate },
+  components: { VueAwesomePaginate, TheNftVue },
 };
 </script>
 
 <style scoped>
+ul {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 16px;
+  list-style-type: none;
+}
+.paginate {
+  margin-top: 50px;
+  margin-bottom: 50px;
+  justify-content: end;
+  display: flex;
+  column-gap: 0px;
+  grid-gap: 0px;
+}
 </style>
